0cea92cb032eb094a6196bbb69ee52f3081782a4
Christine Bonk <christinebonk@gmail.com>
2018-05-05 19:25:47 -0400

    added flip between result and questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,131 @@
 
 //My variables
-var RightCount
-var WrongCount
+var RightCount //Counts number of questions user gets correct
+var WrongCount //Counts number of questions user gets wrong
+var questionNumber = 0;
+var result 
 
 //My questions
 var questions = [
 	{
-	question: "What is the?",
+	question: "In what year was Japan founded?",
 	answers: {
-		a: "Answer 1",
-		b: "Answer 2",
-		c: "Answer 3",
-		d: "Answer 4"
+		a: "1200 CE",
+		b: "660 BC",
+		c: "448 CE",
+		d: "1360 BC"
+	},
+	correctAnswer: "b"
+	},
+
+	{
+	question: "Japan has the highest number of what per capita?",
+	answers: {
+		a: "Vending Machines",
+		b: "Karaoke Bars",
+		c: "Bicycles",
+		d: "Movie Theatres"
+	},
+	correctAnswer: "a"
+	},
+
+	{
+	question: "What does the name Japan mean?",
+	answers: {
+		a: "Oak Tree",
+		b: "Horizontal Beach",
+		c: "Origin of the Sun",
+		d: "Nine provinces"
 	},
 	correctAnswer: "c"
 	},
 
 	{
-	question: "How is the?",
+	question: "What is Japan's highest grossing movie?",
 	answers: {
-		a: "Answer 3",
-		b: "Answer 4",
-		c: "Answer 5",
-		d: "Answer 6"
+		a: "Spirited Away",
+		b: "Titanic",
+		c: "Frozen",
+		d: "Your Name"
 	},
-	correctAnswer: "c"
+	correctAnswer: "a"
+	},
+
+	{
+	question: "How many subway stations are in Tokyo?",
+	answers: {
+		a: "1000",
+		b: "154",
+		c: "77",
+		d: "278"
+	},
+	correctAnswer: "d"
+	},
+
+	{
+	question: "What is the national sport of Japan?",
+	answers: {
+		a: "Baseball",
+		b: "Judo",
+		c: "Jujutsu",
+		d: "Sumo"
+	},
+	correctAnswer: "d"
 	}
 ]
 
-//Display question
-var displayQuestion = function(currentQuestion) {
-	$("h2").text(questions[currentQuestion].question);
+//Display result screen 
+function displayResult(result) {
+
+	//Flip to result
+	$("#questions").addClass("flipfront");
+	$("#results").addClass("flipback");
+
+	//Empty form
 	$("form").empty();
+
+	//Display result
+	if(result) {
+		$("#results").html("Right");
+	} else if(!result) {
+		$("#results").html("Wrong");
+	}
+
+	//Add question number
+	questionNumber += 1; 
+	return
+	//Display Next Question
+	setTimeout(function() { displayQuestion(questionNumber) },5000)
+}
+
+//Display question
+function displayQuestion(currentQuestion) {
+
+	//Flip to Question
+	$("#questions").removeClass("flipfront");
+	$("#results").removeClass("flipback");
+
+	$("h2").text(questions[currentQuestion].question);
+
+	
 	for(letter in questions[currentQuestion].answers) {
 		$("form").append("<label><input type='radio' name='questions' value=" + letter + ">" + questions[currentQuestion].answers[letter] + "</label>");
 		}
+
 	$("form").prepend("<input id='submit' type='submit' value='Submit'>");
+
+	//Timeout
+	setTimeout(displayResult,5000);
 }
 
-displayQuestion(1);
-
-//Question times out 
-
-var displayResult = function(result) {
-	if(result) {
-		$("form").html("You did it!");
-	} else {
-		$("form").html("You didn't do it!");
-	}
-}
-
-//Lose screen 
+//Call first question on load
+displayQuestion(questionNumber);
 
 
 //Determine if user response is right
 var checkAnswer = function(userGuess,correctGuess) {
 	if (userGuess === correctGuess) {
-		RightCount++;
+		RightCount++;	
 		result = true;
 		console.log("right")
 	} else {
@@ -70,8 +139,18 @@ var checkAnswer = function(userGuess,correctGuess) {
 //Collect user response
 $("form").on("click", "#submit", function() {
 	var userGuess = $("input[name=questions]:checked").val();
-	checkAnswer(userGuess,questions[1].correctAnswer);
+	checkAnswer(userGuess,questions[questionNumber].correctAnswer);
 
 })
 
 //Progress bar
+function displayProgress() {
+	for (i=0;i<questions.length;i++) {
+		$("footer").append("<div class='ball'>");
+	}
+}
+
+displayProgress();
+
+//https://api.giphy.com/v1/gifs/search?q=spirited+away&api_key=9yB1iQE0uPH4SpZ4Cf7rnaiWj2SGJKz7
+
